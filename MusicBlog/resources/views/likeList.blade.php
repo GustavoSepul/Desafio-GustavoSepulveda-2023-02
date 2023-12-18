da3e91279205e04f977d4164e43bf093f290d08d
Gustavo Sepulveda <gsepulvedao@ing.ucsc.cl>
2023-12-18 03:10:09 -0300

    listado de canciones favoritas

--- MusicBlog/resources/views/likeList.blade.php
+++ MusicBlog/resources/views/likeList.blade.php
@@ -3,11 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="container">
+            <h1>Listado de canciones favoritas</h1>
+        </div>
         <ul>                
         @forelse (auth()->user()->likes as $like)
-            <li><a href="/songs/{{ $like->song->id }}">{{ $like->song->titulo }}</a></li>
-        @empty
 
+            <li><a href="/songs/{{ $like->song->id }}" class="text-black">{{ $like->song->titulo }}</a></li>
+            
+        @empty
+        
+        <div class="container">
+            <h1>Aún no tienes canciones favoritas, puedes darle me gusta en el siguiente  <a href="/">link</a> </h1>
+        </div>
         @endforelse
         </ul>
     </div>
